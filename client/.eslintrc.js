b977bf3b9471e650a046a1be76bdc54eca6b0154
JoshGoA <josugoar@gmail.com>
2020-04-21 12:51:18 +0200

    Initial commit

---
+++ client/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
---
+++ client/pages/inspire.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <strong>{{ random }}</strong>
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Home',
+  data() {
+    return {
+      random: 0
+    }
+  },
+  methods: {
+    async randomize() {
+      const path = 'http://localhost:5000/api'
+      await this.$axios
+        .get(path)
+        .then((response) => {
+          this.random = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  },
+  created() {
+    this.randomize()
+  }
+})
+</script>

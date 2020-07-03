Softmax activation fix for non categorical output

src/server/model/scripts/utils/bitboard.py
@@ -2,8 +2,7 @@ import chess
 import numpy as np
 
 
-def bitboard(fen: str) -> np.ndarray:
-    board = chess.BaseBoard(fen)
+def bitboard(board: chess.Board) -> np.ndarray:
     bitboard = np.zeros((len(chess.FILE_NAMES), len(chess.RANK_NAMES), len(chess.PIECE_TYPES)))
     for piece_idx, piece_type in enumerate(chess.PIECE_TYPES):
         for file_idx in range(len(chess.FILE_NAMES)):
@@ -11,13 +10,13 @@ def bitboard(fen: str) -> np.ndarray:
                 piece = board.piece_at(file_idx * len(chess.RANK_NAMES) + rank_idx)
                 if piece and piece.piece_type is piece_type:
                     bitboard[file_idx, rank_idx, piece_idx] = 1 if piece.color is chess.WHITE else -1
-    return bitboard
+    return bitboard if board.turn else np.flip(bitboard, axis=np.arange(2)) * -1
+
+
+def print_bitboard(bitboard: np.ndarray) -> str:
+    for piece, idx in zip(chess.PIECE_NAMES[1:], range(bitboard.shape[-1])):
+        print(bitboard[:, :, idx], piece, end="\n\n")
 
 
 if __name__ == "__main__":
-
-    def print_bitboard(bitmap: np.ndarray) -> str:
-        for piece, idx in zip(chess.PIECE_NAMES[1:], range(bitmap.shape[-1])):
-            print(bitmap[:, :, idx], piece, end="\n\n")
-
-    print_bitboard(bitboard(chess.STARTING_BOARD_FEN))
+    print_bitboard(bitboard(chess.Board(chess.STARTING_FEN)))

src/server/model/scripts/worker.py
@@ -1,23 +1,36 @@
-# pylint: disable=import-error
+# pylint: disable=import-error,no-member
 
 import asyncio
+import glob
+import pathlib
 from os import path
 
 import aiofiles
 import numpy as np
 from chess import engine, pgn
+from scipy import special
 
 from utils import bitboard
 
+dir_path = lambda p: path.join(
+    path.dirname(path.realpath(__file__)), p
+)
 
-async def main():
+
+async def worker(path: pathlib.PurePath, *, checkpoint: int = 10000):
+
+    def save_checkpoint():
+        np.savez(
+            dir_path(f"../data/npz/{path.stem}"),
+            **kwds
+        )
+
     kwds = {"X": [], "y": []}
     _, simple_engine = await engine.popen_uci(
         dir_path("../lib/stockfish-11-win/stockfish-11-win/Windows/stockfish_20011801_x64_modern")
     )
-    async with aiofiles.open(
-        dir_path("../data/ficsgamesdb_2019_standard2000_nomovetimes.pgn")
-    ) as f:
+    async with aiofiles.open(path) as f:
+        i = 0
         while True:
             game = pgn.read_game(f._file)
             if not game:
@@ -26,24 +39,36 @@ async def main():
             if mainline:
                 board = np.random.choice(list(mainline)).board()
                 kwds["X"].append(
-                    bitboard(board.board_fen())
+                    bitboard(board)
                 ),
                 kwds["y"].append(
-                    (await simple_engine.analyse(
-                        board,
-                        engine.Limit()
-                    ))["score"].relative.score(mate_score=256)
+                    special.expit(
+                        (await simple_engine.analyse(
+                            board,
+                            engine.Limit(depth=15)
+                        ))["score"].relative.score(mate_score=32768) / 100
+                    )
                 )
+                i += 1
+                if i % checkpoint == 0:
+                    save_checkpoint()
     await simple_engine.quit()
-    np.savez(dir_path("../data/raw/data"), **kwds)
+    save_checkpoint()
 
 
-dir_path = lambda p: path.join(
-    path.dirname(path.realpath(__file__)),
-    p
-)
+async def main():
+    await asyncio.gather(*[
+        worker(pathlib.PurePath(file))
+        for file in glob.glob(
+            dir_path("../data/*.pgn")
+        )
+    ])
 
 
 if __name__ == "__main__":
-    asyncio.set_event_loop_policy(engine.EventLoopPolicy())
-    asyncio.run(main())
+    asyncio.set_event_loop_policy(
+        engine.EventLoopPolicy()
+    )
+    asyncio.run(
+        main(),
+    )